76003172fbff140f81b4ef8bf1aab6c220f85eed
Carlo Jonson <cj811k@att.com>
2015-08-15 09:14:27 -0400

    Do 1st round of cleaning with JSHint & JSBeautifier.  Bring README to required specs.

--- README.md
+++ README.md
@@ -1,34 +1,42 @@
 frontend-nanodegree-arcade-game
 ===============================
+Updated 2015.8.15 9:00am
 
-Doctor Zomboss has sent a special
-horde of minions sporting new rides!
+HOW TO LOAD THE GAME: 
+---------------------
+1) Download or clone the repository here:
+https://github.com/kidpixel/frontend-nanodegree-arcade-game.git
 
-Indeed, these Zomboni warriors are
-not to be taken lightly!
+2) Navigate to the "frontend-nanodegree-arcade-game" folder and double-click/open the index.html file.  This will open your default browser and load the game.
 
-Guide Crazy Dave from the bottom-right
-of the 6x6 game screen to the top row.
-BE WARNED! Although Crazy Dave has 
-special shoes made for walking on ice, 
-some of the Zombonis are WICKED FAST!
+3) You're ready to play the game!  See controls below.
+
+HOW TO PLAY THE GAME: 
+---------------------
+Use the Arrow Keys to navigate Crazy
+Dave Up-Down-Left-Right.
+
+		[Up]
+[Left] [Down] [Right]
+
+
+GAME DESCRIPTION: 
+-----------------
+Doctor Zomboss has sent a special horde of minions sporting new rides!
+Indeed, these Zomboni warriors are not to be taken lightly!
+
+Guide Crazy Dave from the bottom-right of the 6x6 game screen to the top row.
+BE WARNED! Although Crazy Dave has special shoes made for walking on ice, some of the Zombonis are WICKED FAST!
 
 RED Zombonis are angry and the FASTEST.
 YELLOW Zombonis are a moderate speed.
 GREEN Zombonis are slow and nearly blind.
 
 Taco! TACO?
-Of course!  Crazy Dave went all-out 
-trying to get another crack at his taco
-in Plants vs Zombies 2.  
-A tear in the time-space continuum has
-allowed his taco to reappear at random
-along the game board for Crazy Dave to
-re-eat again and again!
-
-...
+Of course!  Crazy Dave went all-out trying to get another crack at his taco in Plants vs Zombies 2.  A tear in the space-time continuum has allowed his taco to reappear at random along the game board for Crazy Dave to re-eat again and again!
 
 NEW FEATURES: 
+-------------
 - Customized ice, grass, & stone tiles.
 - PvZ2 sprites: Zombonis & CRAZY DAVE!
 - PvZ2 soundtrack and sound effects!
@@ -37,3 +45,9 @@ NEW FEATURES:
 - Dave can do a double take looking
   both left and right based on 
   direction travelled. 
+
+
+CONTACT INFO: 
+-------------
+GitHub: kidtexel
+E-mail: cj811k@att.com
--- js/app.js
+++ js/app.js
@@ -9,23 +9,23 @@ var Enemy = function() {
     // x position ... -100 is left off screen starting position
     // 101 (above) times 5 is 505, so I'll send the off screen ones back after x > 505
     this.enitX = 600;
-    this.enitY = this.initializeY(); 
+    this.enitY = this.initializeY();
 
     console.log(this.enitX); // Debug to console.
     console.log(this.enitY); // Made me realize i should return this.y in initializeY
     this.reset();
     this.setSprite();
-}
+};
 
 Enemy.prototype.reset = function() {
-    this.speed = 88 + Math.random()*228;    // Speed calc with min speed + multiplier
-    this.x = this.enitX;                    // x goes back to initial position
-    this.initializeY();                     // get the "row" the enemy will appear on
+    this.speed = 88 + Math.random() * 228; // Speed calc with min speed + multiplier
+    this.x = this.enitX; // x goes back to initial position
+    this.initializeY(); // get the "row" the enemy will appear on
     this.sprite = this.setSprite();
 };
 
 Enemy.prototype.setSprite = function() {
-// The image/sprite for our enemies, this uses
+    // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     console.log('Enemy Speed: ' + this.speed);
     //this.sprite = 'images/enemy-zomboni.png';
@@ -36,17 +36,17 @@ Enemy.prototype.setSprite = function() {
     } else if (this.speed <= 119 && this.speed >= 0) {
         this.sprite = 'images/enemy-zomboni-green.png';
     }
-    return this.sprite; 
+    return this.sprite;
 };
 
-Enemy.prototype.initializeY = function () {
+Enemy.prototype.initializeY = function() {
     // "y" starting point array in 83 pixel increments
-    var eRows = [60,143,309];  // 226 old third row value
+    var eRows = [60, 143, 309]; // 226 old third row value
     // (Debugging comment) Match values to 73, 156, and 239... 13 apart for collision calculation. 
     // generates a random integer between 0 and 2 to select the y row
-    this.y = eRows[Math.round(Math.random()*2)];
-    return this.y;                          
-}
+    this.y = eRows[Math.round(Math.random() * 2)];
+    return this.y;
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -54,13 +54,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    
+
     // Resets the x position of enemies once they go off screen.
     if (this.x > -100) {
-        this.x = this.x - this.speed*dt;  // All that is needed for enemy movement...
+        this.x = this.x - this.speed * dt; // All that is needed for enemy movement...
         // changing to negative reverses the direction of movement
-    }
-    else {
+    } else {
         this.reset();
     }
 
@@ -71,8 +70,8 @@ Enemy.prototype.update = function(dt) {
      * to player width instead.  Around 50-ish seems to be where it feels good yet 
      * still collides.
      */
-    if ( (Math.abs(player.x-this.x)<80) && (player.y === (this.y+13)) ) {
-        console.log('Collision. ' + this.x + '(' + Math.round(this.x) +') '+ this.y + '(' + (this.y+13) +')'); // this debugging line kept me sane
+    if ((Math.abs(player.x - this.x) < 80) && (player.y === (this.y + 13))) {
+        console.log('Collision. ' + this.x + '(' + Math.round(this.x) + ') ' + this.y + '(' + (this.y + 13) + ')'); // this debugging line kept me sane
         this.killSound.play();
         player.reset();
     }
@@ -82,7 +81,7 @@ Enemy.prototype.update = function(dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // functions to make: reset update render... 
 
@@ -95,37 +94,37 @@ var gemClass = function() {
     this.audio = [];
     this.audio[0] = new Audio('audio/pvz-tacocrunch.mp3');
     this.audio[1] = new Audio('audio/pvz-zombiescoming.mp3');
-    this.audio[1].play(); 
+    this.audio[1].play();
     this.enitX = 404; // Starting x value for taco. 
-    this.speed = 0; 
-    this.sprite = 'images/gem-daves-taco.png'; 
+    this.speed = 0;
+    this.sprite = 'images/gem-daves-taco.png';
     this.reset();
-}
+};
 // Sets speed, x, calls for y, and sets sprite image
-gemClass.prototype.reset = function () {
+gemClass.prototype.reset = function() {
     console.log('reset');
-    var eRows = [60,143,309];  // 226 old third row value
+    var eRows = [60, 143, 309]; // 226 old third row value
     // Move the taco along the x-axis on reset 101 to 404 to the left of enitX
-    this.x = this.enitX - (101*Math.round(Math.random()*3)); 
+    this.x = this.enitX - (101 * Math.round(Math.random() * 3));
     // Reverence: Math.round(Math.random()*2)  does 0-2, so 3 values
-    this.y = eRows[Math.round(Math.random()*2)];  
-}
+    this.y = eRows[Math.round(Math.random() * 2)];
+};
 
 // 
 // Collision gets calculated here
-gemClass.prototype.update = function (dt) {
-    console.log('update'); 
-    if ( (Math.abs(player.x-this.x)<50) && (player.y === (this.y+13)) ) {
-    console.log('TACOLLISION. ' + this.x + '(' + Math.round(this.x) +') '+ this.y + '(' + (this.y+13) +')'); 
-    this.audio[0].play(); // play taco crunch sound
-    // player.reset();  in this case don't reset player, but increment a 'tacowin'
-    this.reset();   
-    } 
-}
+gemClass.prototype.update = function() {
+    console.log('update');
+    if ((Math.abs(player.x - this.x) < 50) && (player.y === (this.y + 13))) {
+        console.log('TACOLLISION. ' + this.x + '(' + Math.round(this.x) + ') ' + this.y + '(' + (this.y + 13) + ')');
+        this.audio[0].play(); // play taco crunch sound
+        // player.reset();  in this case don't reset player, but increment a 'tacowin'
+        this.reset();
+    }
+};
 
-gemClass.prototype.render = function () {
+gemClass.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-} 
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -148,15 +147,15 @@ var playerClass = function() {
         this.play();
     }, false);
     myAudio.play();
-    */   // working code for background music
+    */ // working code for background music
     this.initX = 505;
-    this.initY = 405; 
+    this.initY = 405;
     this.reset();
 };
 
-playerClass.prototype.update = function(){
+playerClass.prototype.update = function() {
 
-// I could use an allEnemies.forEach call and iterate through the enemies array here (like in the Engine.js updateEntities, but instead of doing all that, we can 'hijack' Enemy.prototype.update since we're checking for coordinate conditions there, too.)
+    // I could use an allEnemies.forEach call and iterate through the enemies array here (like in the Engine.js updateEntities, but instead of doing all that, we can 'hijack' Enemy.prototype.update since we're checking for coordinate conditions there, too.)
 
 };
 
@@ -167,34 +166,42 @@ playerClass.prototype.render = function() {
 
 playerClass.prototype.reset = function() {
     this.x = this.initX;
-    this.y = this.initY; 
+    this.y = this.initY;
 };
 
 playerClass.prototype.handleInput = function(allowedKeys) {
 
-    if(allowedKeys === 'left') {
+    if (allowedKeys === 'left') {
         this.x = this.x - 101;
         this.hero = 'images/char-crazydave-left.png';
-        if (this.x < 0) { this.x = this.x + 101 };
+        if (this.x < 0) {
+            this.x = this.x + 101;
+        }
     }
-    if(allowedKeys === 'right') {
+    if (allowedKeys === 'right') {
         this.x = this.x + 101;
         this.hero = 'images/char-crazydave-right.png';
-        if(this.x > 505) { this.x = this.x - 101 };
+        if (this.x > 505) {
+            this.x = this.x - 101;
+        }
     }
-    if(allowedKeys === 'down') {
+    if (allowedKeys === 'down') {
         this.y = this.y + 83;
-        if(this.y > 405) { this.y = this.y - 83 };
+        if (this.y > 405) {
+            this.y = this.y - 83;
+        }
     }
-    if(allowedKeys === 'up') { this.y = this.y - 83 };
+    if (allowedKeys === 'up') {
+        this.y = this.y - 83;
+    }
 
-    if(this.y < 50) { 
+    if (this.y < 50) {
         this.audio[1].play(); // yehaa
         this.reset();
         console.log("Victory!");
     }
 
-console.log('Player ' + this.x + ' ' + this.y); // For player debug purposes
+    console.log('Player ' + this.x + ' ' + this.y); // For player debug purposes
 };
 
 
@@ -203,14 +210,16 @@ console.log('Player ' + this.x + ' ' + this.y); // For player debug purposes
 // Place the player object in a variable called player
 
 var allEnemies = [];
-for(var enemies = 0; enemies < 8; enemies++) {
+for (var enemies = 0; enemies < 8; enemies++) {
     // Placing the taco first in array sends it to the 'back' of the stack.
-    if (enemies === 0) { allEnemies.push(new gemClass)};
+    if (enemies === 0) {
+        allEnemies.push(new gemClass());
+    }
     allEnemies.push(new Enemy());
 } // After creating the array of enemies, create a few and push instances of Enemy object into it.
 
-var player = new playerClass;
-var gem = new gemClass;
+var player = new playerClass();
+// var gem = new gemClass();  // Didn't end up needing/using this.
 
 
 // This listens for key presses and sends the keys to your
